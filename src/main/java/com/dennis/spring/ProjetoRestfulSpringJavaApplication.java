4a4133af174e62e97d79f4413ebf34ee13cf21bc
Dennis Silveira <dennis.silveira@dataprev.gov.br>
2019-05-15 14:10:48 -0300

    subir projeto completo

---
+++ src/main/java/com/dennis/spring/ProjetoRestfulSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dennis.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRestfulSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRestfulSpringJavaApplication.class, args);
+	}
+
+}
